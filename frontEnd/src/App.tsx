cc42becd7bd30bfbee0fa1b0f18e3801d38543ad
Elbert Nathanael <elbertwithane@gmail.com>
2024-09-27 10:41:22 +0800

    updated front end

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -2,19 +2,22 @@ import { useEffect, useState } from "react";
 import VideoBlock from "./components/videoBlock";
 
 export default function App() {
-  const [contentData, setContentData] = useState<any>(
-    {date:"yy-mm-dd", mainSummary:"oh boy we screwed up somewhere"}
-  )
+  const [contentData, setContentData] = useState<any>({
+    date: "yy-mm-dd",
+    mainSummary: "oh boy we screwed up somewhere",
+  });
 
   // load data from server
-  useEffect(()=>{
-    (async ()=>{
-      const data = await fetch("https://elcloudapp.com/forward?to=http://13.214.102.62:4000/currentUpdate")
-      setContentData(await data.json())
-    })()
-  }, [])
+  useEffect(() => {
+    (async () => {
+      const data = await fetch(
+        "https://elcloudapp.com/forward?to=http://18.143.103.236:4000/currentUpdate"
+      );
+      setContentData(await data.json());
+    })();
+  }, []);
 
-  const contentKeys = contentData ? Object.keys(contentData) : []
+  const contentKeys = contentData ? Object.keys(contentData) : [];
 
   return (
     <div className="main border">
@@ -25,22 +28,34 @@ export default function App() {
       {/* subtitle */}
       <div className="subtitle">
         <div>
-          <h2 className="paddingRem noto border">What happened this week in innovative tech</h2>
+          <h2 className="paddingRem noto border">
+            What happened this week in innovative tech
+          </h2>
           <h2 className="paddingRem noto border">{contentData.date}</h2>
         </div>
-        <p className="weekSummary paddingRem border">{contentData.mainSummary}</p>
+        <p className="weekSummary paddingRem border">
+          {contentData.mainSummary}
+        </p>
       </div>
       <hr />
       {/* main content */}
       <div className="contentContainer">
         {}
-        {contentKeys!.map((key)=>{
-          if(key === "date" || key === "mainSummary"){return null}
-          const dataBlock:any = contentData![key]
-          return <VideoBlock title={key} summary={dataBlock.summary} id={dataBlock.id!}/>
+        {contentKeys!.map((key) => {
+          if (key === "date" || key === "mainSummary") {
+            return null;
+          }
+          const dataBlock: any = contentData![key];
+          return (
+            <VideoBlock
+              title={key}
+              summary={dataBlock.summary}
+              id={dataBlock.id!}
+            />
+          );
         })}
       </div>
       <hr />
     </div>
-  )
+  );
 }
